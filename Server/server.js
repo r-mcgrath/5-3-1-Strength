6897c2aaf35ddb80add4cc96b0c3c8c62ce45db1
r-mcgrath <97359037+r-mcgrath@users.noreply.github.com>
2024-04-04 11:10:14 -0400

    add packages

---
+++ Server/server.js
@@ -0,0 +1,32 @@
+// require libraries
+const express = require('express');
+
+// require controllers
+
+
+// require router
+
+// set constants
+const PORT = 3333;
+
+// create HTTP request handler by calling express() and saving result in variable named app
+const app = express();
+// parse requests
+
+// serve static assets
+
+// serve the index.html file from the views folder at the root 
+
+
+// serve blog.html file from the views folder to /blog
+
+// route handler for entries
+
+// TODO: add a route handler that is a catchall for invalid routes (404 error handler)
+
+// global error handler
+
+// start server and listen on assigned port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
